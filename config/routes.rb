a2f737a171e74defc9888e4ea88238e4fd96acde
Everton Leon <evertonleon07@gmail.com>
2025-01-19 11:03:27 -0300

    feat: Criar teste para usuários

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  
   root :to => "products#index"
 
   devise_for :users
