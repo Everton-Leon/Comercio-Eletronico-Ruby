362b39c778352fa9a212741b9ce342e0c7e4588a
Everton Leon <evertonleon07@gmail.com>
2025-02-12 16:36:08 -0300

    feat: finalizar carrinho de compras

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
     def add_to_cart
-        product_id = params[:product]
+        product_id = params[:product_id]
         session[:cart] ||= {} # Inicializa o carrinho na sessão, se não existir
         session[:cart][product_id] ||= 0
         session[:cart][product_id] += 1
