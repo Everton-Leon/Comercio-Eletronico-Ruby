215b92ffbb7de4bcb60de2c60044641633dedd58
Everton Leon <evertonleon07@gmail.com>
2025-02-13 09:12:24 -0300

    fix: corrigir carrinho

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,8 @@
 class CartsController < ApplicationController
     def add_to_cart
         product_id = params[:product_id]
-        session[:cart] ||= {} # Inicializa o carrinho na sessão, se não existir
-        session[:cart][product_id] ||= 0
-        session[:cart][product_id] += 1
+        session[:cart] ||= {}
+        session[:cart][product_id] = session[:cart][product_id].to_i
         redirect_to products_path, notice: "Produto adicionado ao carrinho!"
       end
 
